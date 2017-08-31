4b6639a9e039681a440834711cb681fbb7638d74
Eric Peterson <epeterson@interpid.io>
2017-08-31 14:53:28 -0400

    Added logging hope it works

--- command.php
+++ command.php
@@ -35,14 +35,20 @@
 	if ($destination == 'view all') {
 		$train_msg = 'Check out these trains before they leave the station!' . "\r\n";
 		$train_ids = get_all_train_ids();
+		error_log('All train ids count = '.count($train_ids).' train ids are: '.$train_ids);
 		if (count($train_ids) == 0) {
+			error_log('There are no train ids :(');
 			$train_msg = 'Chew Hoo! There are no lunch trains yet. You should make one!';
 		} else {
+			error_log('There are train ids!');
 			foreach ($ids as $id) {
+				error_log('Looking at train id -> '.$id);
 				$ret = get_train_by_id($id);
 				$train = $ret['train'];
+				error_log('Train destination is: '.$train['destination']);
 				$train_destination = 'To ' . $train['destination'] . ' at ' . $train['date_leaving'] . "\r\n";
 				$train_msg .= $train_destination;
+				error_log('Train message = '.$train_msg);
 			}
 		}
 		$message = array(
